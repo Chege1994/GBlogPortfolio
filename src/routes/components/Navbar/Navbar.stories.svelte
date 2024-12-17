af0b0f1f8a62fb85aa220d7edf1282390f4cc301
Chege1994 <cppdgc@gmail.com>
2024-12-17 20:09:53 +0100

    added storybook

---
+++ src/routes/components/Navbar/Navbar.stories.svelte
@@ -0,0 +1,31 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Navbar from './Navbar.svelte';
+	import { fn } from '@storybook/test';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'New/Navbar',
+		component: Navbar,
+		tags: ['autodocs'],
+		argTypes: {
+			backgroundColor: { control: 'color' },
+			size: {
+				control: { type: 'select' },
+				options: ['small', 'medium', 'large']
+			}
+		},
+		args: {
+			onClick: fn()
+		}
+	});
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Primary" args={{ primary: true, label: 'Navbar' }} />
+
+<Story name="Secondary" args={{ label: 'Navbar' }} />
+
+<Story name="Large" args={{ size: 'large', label: 'Navbar' }} />
+
+<Story name="Small" args={{ size: 'small', label: 'Navbar' }} />
