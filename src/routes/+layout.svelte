cb44aeb3301d83e49ee379b1101bb265f1eb4c77
Chege1994 <57599157+Chege1994@users.noreply.github.com>
2024-12-17 11:42:12 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,20 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+
+	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<nav class="bg-gray-800 p-4 text-white">
+<nav class="bg- flex justify-between bg-gray-800 p-4 text-center align-middle text-white">
 	<ul class="flex space-x-4">
 		<li><a href="/" class="hover:underline">Home</a></li>
 		<li><a href="/projects" class="hover:underline">Projects</a></li>
 		<li><a href="/blog" class="hover:underline">Blog</a></li>
 		<li><a href="/tools/converter" class="hover:underline">Tools</a></li>
 	</ul>
+	<ul>
+		<li><DarkMode /></li>
+	</ul>
 </nav>
+
 {@render children()}
